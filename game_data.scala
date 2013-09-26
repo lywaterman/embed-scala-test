d47f157740f5eafe47962b339762ea124bf9b8de
ubuntu-server <liuyang@ubuntu-server-64>
2013-09-26 15:29:05 +0800

    fix bug

--- game_data.scala
+++ game_data.scala
@@ -1,1 +1,24 @@
+object game_data {
+  var x = 1
+  def fib(n:Int) : Int = {
+    if (n<2) {
+      n
+    }else
+    {
+      fib(n-1) + fib(n-2)
+    }
+  }
+
+  def time[A](a: => A) = {
+		val now = System.nanoTime
+		val result = a
+		val micros = (System.nanoTime - now) / 1e6
+		println("%f microseconds".format(micros))
+		result
+	}
+}
+
+class game_data {
+
+}
 
