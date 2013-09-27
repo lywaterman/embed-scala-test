594bd8085fdab25ddbeec74dda63ef0d54f868bb
ubuntu-server <liuyang@ubuntu-server-64>
2013-09-27 11:16:13 +0800

    add

--- Sample2.java
+++ Sample2.java
@@ -10,8 +10,10 @@ public class Sample2 {
 	public static String sayHello(String name) {
     	Eval eval = new Eval();
 		
+		System.out.println("compile game_data");
 		eval.compile(eval.toSource(new File("game_data.scala")));
 
+		System.out.println("run game_func");
     	java.lang.Object value = eval.apply(eval.toSource(new File("game_func.scala")), false);
 
 		System.out.println(value.getClass().getName());
